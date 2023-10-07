122527401cc63a8b98dd15a4602687249b72fbb5
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-10-07 20:25:19 +0800

    redactor: replace sql to xml

--- src/main/java/com/example/springmybatis/repository/TeacherMapper.java
+++ src/main/java/com/example/springmybatis/repository/TeacherMapper.java
@@ -7,31 +7,13 @@ import java.util.List;
 
 @Mapper
 public interface TeacherMapper {
-  @Select("SELECT * FROM teacher")
   List<Teacher> selectAll();
 
-  @Select("""
-      SELECT * FROM teacher
-      WHERE id = #{id}
-    """)
   Teacher selectByPrimaryKey(Long id);
 
-  @Insert("""
-      INSERT INTO teacher (user_name, email)
-      VALUES (#{userName}, #{email})
-    """)
   int insert(Teacher record);
 
-  @Update("""
-      UPDATE teacher
-      SET user_name = #{userName}, email = #{email}
-      WHERE id = #{id}
-    """)
   int updateByPrimaryKey(Teacher record);
 
-  @Delete("""
-      DELETE FROM teacher
-      WHERE id = #{id}
-    """)
   int deleteByPrimaryKey(Long id);
 }
