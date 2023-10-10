28facc4f60214b227db5cb235ae7ed9a0a15ed95
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-10-10 22:44:01 +0800

    feat: add evergreen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,7 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Alert, Pane} from "evergreen-ui";
 
 function App() {
-  const [count, setCount] = useState(0)
   const [data, setData] = useState<any>(null);
 
   useEffect(() => {
@@ -19,26 +16,32 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Pane>
+        <Alert
+          intent="none"
+          title="There are over 200 integrations available"
+          marginBottom={32}
+        />
+        <Alert
+          intent="success"
+          title="Your source is now sending data"
+          marginBottom={32}
+        />
+        <Alert
+          intent="warning"
+          title="Changes will affect all warehouses"
+          marginBottom={32}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed dod eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Alert>
+        <Alert intent="danger"
+               title="We weren’t able to save your changes"
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed dod eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Alert>
+      </Pane>
     </>
   )
 }
