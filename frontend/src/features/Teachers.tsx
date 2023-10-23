0c1e816e8b5ecfa01fd700217b31927c92afbb32
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2023-10-24 00:57:38 +0800

    fix: top design

--- frontend/src/features/Teachers.tsx
+++ frontend/src/features/Teachers.tsx
@@ -31,6 +31,9 @@ export const Teachers = () => {
         <Loader />
       ) : (
         <>
+          <Button marginBottom={16} iconAfter={PlusIcon} onClick={navCreate}>
+            New Teacher
+          </Button>
           <Table.Body>
             <Table.Head>
               <Table.TextCell>ID</Table.TextCell>
@@ -41,54 +44,43 @@ export const Teachers = () => {
             <Table.Body>
               {data &&
                 data.map((item) => (
-                  <Table.Row key={item.id}>
+                  <Table.Row key={item.id} height={100}>
                     <Table.TextCell>{item.id}</Table.TextCell>
                     <Table.TextCell>{item.userName}</Table.TextCell>
                     <Table.TextCell>{item.email}</Table.TextCell>
-                    <Table.TextCell textAlign={'right'}>
-                      <Button
-                        marginY={8}
-                        marginRight={12}
-                        iconBefore={EditIcon}
-                        onClick={() => navEdit(item)}
-                      >
-                        Edit
-                      </Button>
-                      <Button
-                        marginY={8}
-                        marginRight={12}
-                        iconBefore={TrashIcon}
-                        intent="danger"
-                        onClick={() => handleDelete(item.id)}
-                      >
-                        Delete
-                      </Button>
+                    <Table.TextCell>
+                      <Pane display={'flex'} flexDirection={'column'}>
+                        <Button
+                          marginY={8}
+                          iconBefore={EditIcon}
+                          onClick={() => navEdit(item)}
+                        >
+                          Edit
+                        </Button>
+                        <Button
+                          marginY={8}
+                          iconBefore={TrashIcon}
+                          intent="danger"
+                          onClick={() => handleDelete(item.id)}
+                        >
+                          Delete
+                        </Button>
+                      </Pane>
                     </Table.TextCell>
                   </Table.Row>
                 ))}
             </Table.Body>
           </Table.Body>
-          <Pane
+          <Pagination
             display={'flex'}
-            justifyContent={'space-between'}
-            alignItems={'center'}
-          >
-            <Button
-              marginY={8}
-              marginRight={12}
-              iconAfter={PlusIcon}
-              onClick={navCreate}
-            >
-              New Teacher
-            </Button>
-            <Pagination
-              page={page + 1}
-              totalPages={totalPages}
-              onPreviousPage={() => setPage((prevPage) => prevPage - 1)}
-              onNextPage={() => setPage((prevPage) => prevPage + 1)}
-              onPageChange={setPage}
-            ></Pagination>
-          </Pane>
+            justifyContent={'center'}
+            marginTop={16}
+            page={page + 1}
+            totalPages={totalPages}
+            onPreviousPage={() => setPage((prevPage) => prevPage - 1)}
+            onNextPage={() => setPage((prevPage) => prevPage + 1)}
+            onPageChange={setPage}
+          />
         </>
       )}
     </Pane>
